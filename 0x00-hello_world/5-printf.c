bfe4657dbc7430404f094ee61cf8044d04bdc10d
ngatiabenson <mukuhabenson@gmail.com>
2023-06-15 05:26:44 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,11 @@
-#include<stdio.h>
-int main()
+#include <stdio.h>
+
+/**
+ *main- program starting point
+ *Return: 0
+ */
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-   return 0;
+	return (0);
 }
