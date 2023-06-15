42c9c1b8f94541d15f30ff646a08b42931e53094
ngatiabenson <mukuhabenson@gmail.com>
2023-06-15 05:15:17 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+* main -  starting  point
+*Description: outputs "\"Programming is like building a multilingual puzzle"
+*Return: 0
+*/
+
+int main(void)
+
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
