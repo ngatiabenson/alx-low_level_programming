5520c4817c516d2469f8462ef8226026092bf7a8
ngatiabenson <mukuhabenson@gmail.com>
2023-06-18 06:36:06 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ *main - entry point
+ *Description: 1-9
+ * Return: 0
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+	{
+		printf("%d", x);
+	}
+	printf("\n");
+	return (0);
+}
+
