273c96d8528c5e2241e3ae69b3e0989af81235eb
ngatiabenson <mukuhabenson@gmail.com>
2023-06-21 03:42:26 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_alphabet  - prints the lowercase alphabets
+ */
+void print_alphabet(void)
+{
+	char la;
+	for (la = 'a'; la <= 'z'; la++)
+		_putchar(la);
+	_putchar('\n')
+	return (0);
+}
