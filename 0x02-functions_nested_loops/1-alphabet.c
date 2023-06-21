e7369f4f5c5d616b5f30049e3a73ca97d9a24064
ngatiabenson <mukuhabenson@gmail.com>
2023-06-21 01:57:47 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ *print_alphabet - entry
+ *
+ *Description: print the alphabet
+ *Return: 0
+ */
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		_putchar(letter);
+	}
+	_putchar('\n');
+}
+
