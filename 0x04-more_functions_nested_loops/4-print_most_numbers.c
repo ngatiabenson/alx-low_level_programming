cc37ae3fb687a46bcf08ba05450da141e6c0719c
ngatiabenson <mukuhabenson@gmail.com>
2023-06-22 07:00:15 -0700

    ./4-print_most_numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,15 +2,18 @@
 /**
  * print_most_numbers - prints 01356789
  * return: void
+ * @c:the character to be searched
  */
 
 void print_most_numbers(void)
 {
 char c;
-for (c = '0'; c <= '9'; c++)
-{
-if (!(c == '2' || c == '4'))
-_putchar(C);
+
+	for (c = '0'; c <= '9'; c++)
+	{
+	if (!(c == '2' || c == '4'))
+	_putchar(c);
+	}
+	_putchar('\n');
 }
-_putchar('\n');
-}
+
