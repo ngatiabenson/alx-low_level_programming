6119c3fd03072dc70a06308f5979910b035e5a54
ngatiabenson <mukuhabenson@gmail.com>
2023-07-03 01:32:42 -0700

    1-memcpy.c

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * @dest: memory where its stored
+ * @src: memor where copied
+ * @n: number of bytes
+ * return: copied memory with n bytes changed
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int r = 0;
+	int i = n;
+
+	for  (; r < i; r++)
+	{
+		destr[r] = src[r];
+		n--;
+	}
+	return (dest);
+}
