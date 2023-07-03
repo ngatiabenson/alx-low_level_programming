e4cedda5763bba833de30972c78347d91b6a73ca
ngatiabenson <mukuhabenson@gmail.com>
2023-07-03 02:52:09 -0700

    ./100-set_string

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * set_string- sets value of pointer to character
+ * @s: pointer to pointer
+ * @to: pointer to character
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
