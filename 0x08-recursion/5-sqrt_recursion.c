0639a565983e90fba5abda32d1df9ef0b06bdcd4
ngatiabenson <mukuhabenson@gmail.com>
2023-07-08 22:29:47 -0700

    5-sqrt_recursion.c

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _evaluate - evaluates the funtion sqrt
+ * @i: integer
+ * @n:integer
+ * return: evaluate sqrt
+ */
+
+int _evaluate(int i, int n)
+{
+if (n == 0 || n == 1)
+return (n);
+else if (i * i < n)
+return  (_evaluate(i + 1, n));
+else if (i * i == n)
+return (i);
+return (-1);
+return (-1);
+}
