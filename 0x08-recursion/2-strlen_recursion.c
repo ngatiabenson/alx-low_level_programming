05028de431f250385d49a18a8c89dd6288b2c298
ngatiabenson <mukuhabenson@gmail.com>
2023-07-22 22:52:49 -0700

    exercise two

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -4,10 +4,12 @@
  * @s:pointer blockto fil memory
  * return:srtlen_recursion
  */
-int _srtlen_recursion(char *s)
+int _strlen_recursion(char *s)
 {
-if (*s == '\0')
-return (0);
-else
-return (1 + _srtlen_recursion(s + 1));
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	s++;
+	return (_strlen_recursion(s) + 1);
 }
