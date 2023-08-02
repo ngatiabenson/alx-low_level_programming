ceb4e19a09c4b5470e7f1f77a9bea31aba84a3b0
ngatiabenson <mukuhabenson@gmail.com>
2023-08-02 05:33:11 -0700

    . File name

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - prints name of file
- * return: Always zero upon success
+ * main - prints the name of the file
+ *
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-	printf("%s\n", _FILE_);
+	printf("%s\n", __FILE__);
 	return (0);
 }
